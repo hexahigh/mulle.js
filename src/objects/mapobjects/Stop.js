e95b1e9067ad7c8e31461bf354185eef435d617d
datagutten <datagutten@datagutten.net>
2020-06-13 13:51:31 +0200

    Add road blocks

--- src/objects/mapobjects/Stop.js
+++ src/objects/mapobjects/Stop.js
@@ -1,12 +1,17 @@
 'use strict'
 
-var MapObject = {}
+const MapObject = {}
 
+// End of the world/closed road
 MapObject.onCreate = function () {
-  console.error('unfinished object', this.id, this)
+    let direction = this.opt['Direction'];
+    this.setDirectorMember(this.def.FrameList[direction][0]);
+    console.log(this.def.FrameList);
 }
 
-MapObject.onEnterInner = function () {
+MapObject.onEnterInner = function (car) {
+    car.speed = 0;
+    car.stepback(2);
 
 }
 
