2e37326da7a8570dc00d86b32eb9b5cd04c8acbc
datagutten <datagutten@datagutten.net>
2020-06-14 22:13:37 +0200

    Raise exception if path is not found

--- assets.py
+++ assets.py
@@ -233,6 +233,8 @@ if not os.path.exists(assetOutPath):
 	os.makedirs(assetOutPath)
 assetWebPath = "assets"
 resourcePath = 'cst_out_new'
+if not os.path.exists(resourcePath):
+	raise FileNotFoundError(resourcePath)
 meta = {}
 
 assetIndex = {}
