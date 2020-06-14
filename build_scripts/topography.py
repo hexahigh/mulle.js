ac0b6d74451f3202079cd866c14f35292119a3f8
datagutten <datagutten@datagutten.net>
2020-06-14 12:59:59 +0200

    Add build script for topography

---
+++ build_scripts/topography.py
@@ -0,0 +1,46 @@
+import json
+import os
+import subprocess
+
+from PIL import Image, ImageDraw
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+project_path = os.path.realpath(os.path.join(dir_path, '..'))
+topo_path = os.path.join(project_path, "dist", "assets", "topography")
+topo_source = os.path.join(project_path, "cst_out_new", "CDDATA.CXT", "Standalone")
+
+if not os.path.exists(topo_path):
+    os.mkdir(topo_path)
+
+metadata = json.load(open(os.path.join(topo_source, "..", "metadata.json")))
+for num in range(693, 748 + 1, 2):
+    data = metadata["libraries"][0]["members"][str(num)]
+    data2 = metadata["libraries"][0]["members"][str(num + 1)]
+    if data["name"] != data2["name"][:-2]:
+        raise ValueError("Name mismatch")
+
+    with open(os.path.join(topo_source, str(num) + ".txt"), "rb") as fp:
+        data_string = fp.read()
+    with open(os.path.join(topo_source, str(num + 1) + ".txt"), "rb") as fp:
+        data_string += fp.read()
+
+    im = Image.new("P", [316, 198])
+    im.putpalette([0xF0, 0xF0, 0xF0, 0x00, 0x00, 0x00])
+
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([(0, 0), (315, 197)], 0)
+
+    y = 0
+    x = 0
+    for byte in data_string:
+        if byte != 0xF0:
+            draw.point([x, y], 1)
+        x += 1
+        if x >= 316:
+            y += 1
+            x = 0
+
+    im.save(os.path.join(topo_path, data["name"] + ".png"))
+
+project_file = os.path.join(dir_path, 'topography.ftpp')
+subprocess.call(['npx', 'free-tex-packer-cli', '--project', project_file, '--output', topo_path])
