229f2a752f5ad63bada3d546b5865029d16d3200
datagutten <datagutten@datagutten.net>
2020-06-15 18:54:33 +0200

    Check if file exists before mounting

--- deployment/mount.ps1
+++ deployment/mount.ps1
@@ -1,3 +1,8 @@
-﻿Mount-DiskImage "$HOME\mullebil.iso"
+﻿if (-not (Test-Path "$HOME\mullebil.iso" -PathType leaf) ) {
+Throw "ISO file not found at $HOME\mullebil.iso"
+}
+
+Test-Path "$HOME\mullebil.iso"
+Mount-DiskImage "$HOME\mullebil.iso"
 $letter = (Get-DiskImage "$HOME\mullebil.iso"| get-volume).DriveLetter + ':'
 cmd /c mklink /d $HOME\mullebil $letter
