c0e6f4a50970bbccad307a3bf89c672bc85f4a2b
datagutten <datagutten@datagutten.net>
2020-06-17 21:12:19 +0200

    Extract different case

--- extract.py
+++ extract.py
@@ -7,9 +7,15 @@ gameDir = sys.argv[1]
 if not os.path.exists(gameDir):
     raise FileNotFoundError('Game directory %s not found' % gameDir)
 gameDir = os.path.realpath(gameDir)
-gameDir = os.path.join(gameDir, 'MOVIES')
+if os.path.exists(os.path.join(gameDir, 'MOVIES')):
+    gameDir = os.path.join(gameDir, 'MOVIES')
+    files = glob('%s%s*.CXT' % (gameDir, os.path.sep)) + glob('%s%s*.DXR' % (gameDir, os.path.sep))
+elif os.path.join(gameDir, 'movies'):
+    gameDir = os.path.join(gameDir, 'movies')
+    files = glob('%s%s*.dxr' % (gameDir, os.path.sep)) + glob('%s%s*.dxr' % (gameDir, os.path.sep))
+else:
+    raise FileNotFoundError('movies directory not found at %s' % gameDir)
 
-files = glob('%s%s*.CXT' % (gameDir, os.path.sep)) + glob('%s%s*.DXR' % (gameDir, os.path.sep))
 if not files:
     raise FileNotFoundError('No files found in %s' % gameDir)
 
