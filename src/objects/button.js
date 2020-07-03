e28127074c1adb255922ef28ace6db764a5e6c02
datagutten <datagutten@datagutten.net>
2020-07-03 21:51:48 +0200

    Add method to create a button without texture

--- src/objects/button.js
+++ src/objects/button.js
@@ -1,3 +1,4 @@
+/* global Phaser */
 /**
  * Button extension
  * @module objects/button
@@ -19,24 +20,42 @@ class MulleButton extends Phaser.Button {
     this.displaySprite = new MulleSprite(this.game, this.x, this.y)
 
     // this.displaySprite.loadTexture( this.opt.imageDefault[0], this.opt.imageDefault[1].toString() );
-    this.displaySprite.setDirectorMember(this.opt.imageDefault[0], this.opt.imageDefault[1])
+    if (this.opt.imageDefault) {
+      this.displaySprite.setDirectorMember(this.opt.imageDefault[0], this.opt.imageDefault[1])
 
-    this.game.add.existing(this.displaySprite)
+      this.game.add.existing(this.displaySprite)
 
-    if (this.displaySprite._frame) {
-      this.hitArea = new Phaser.Rectangle(
-        -this.displaySprite.regPoint.x,
-        -this.displaySprite.regPoint.y,
-        this.displaySprite._frame.width,
-        this.displaySprite._frame.height
-      )
-    } else {
-      console.error('no hit area', this)
+      if (this.displaySprite._frame) {
+        this.hitArea = new Phaser.Rectangle(
+          -this.displaySprite.regPoint.x,
+          -this.displaySprite.regPoint.y,
+          this.displaySprite._frame.width,
+          this.displaySprite._frame.height
+        )
+      } else {
+        console.error('no hit area', this)
+      }
     }
 
     this.input.useHandCursor = false
 
     this.cursor = 'Click'
+  }
+
+  /**
+   * Create a button without a texture
+   * @param {Phaser.Game} game
+   * @param {int} x Left
+   * @param {int} y Top
+   * @param {int} w Width
+   * @param {int} h Height
+   * @param opt Options
+   */
+  static fromRectangle (game, x, y, w, h, opt) {
+    const button = new MulleButton(game, x, y, opt)
+    button.width = w
+    button.height = h
+    return button
   }
 
   onInputOverHandler () {
