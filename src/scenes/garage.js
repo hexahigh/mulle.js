8ec9449cea6b112f51fbc166f7f923c46478cec0
datagutten <datagutten@datagutten.net>
2020-07-03 12:20:55 +0200

    Implement "Trash your car"-button in garage toolbox

--- src/scenes/garage.js
+++ src/scenes/garage.js
@@ -453,7 +453,7 @@ class GarageState extends MulleState {
 
       const funcList = {
         Trash: () => {
-          alert('Trash your car')
+          this.car.trash()
         },
         Diploma: () => {
           alert('Show diploma')
