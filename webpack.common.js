bfc11ff35290d12977c55cadcc70f3d6142aafaf
datagutten <datagutten@datagutten.net>
2020-07-07 16:12:11 +0200

    Replace deprecated uglifyjs-webpack-plugin with terser-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,6 +1,5 @@
 // const webpack = require('webpack')
 const path = require('path')
-// const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
 
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,30 @@
 const webpack = require('webpack')
 const { merge } = require('webpack-merge')
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 const common = require('./webpack.common.js')
 
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin({
+      terserOptions: {
+        compress: {
+          pure_funcs: ['console.debug']
+        }
+      }
+    }
+    )
+    ]
+  },
+
   plugins: [
 
     new webpack.DefinePlugin({
       'process.env': {
-        'NODE_ENV': JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production')
       }
-    }),
-
-    new UglifyJSPlugin({
-      // minimize: true
-
-      compress: {
-        pure_funcs: ['console.debug']
-      }
-
     })
-
   ]
 
 })
