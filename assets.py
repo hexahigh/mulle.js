48c511541bf314e1ae165ab5971d357de532b426
datagutten <datagutten@datagutten.net>
2020-07-19 11:41:52 +0200

    Make optipng work on linux

--- assets.py
+++ assets.py
@@ -447,8 +447,8 @@ for res in MulleResources:
 			PyTexturePackerUtils.save_image(packed_image, assetOutPath + "/" + atlasName + '.png')
 
 			if optimizeImages > 0:
-				call('optipng.exe -o' + str( optimizeImages ) + ' ' + assetOutPath + "/" + atlasName + '.png')
-			
+				call(['optipng', '-o', str(optimizeImages), assetOutPath, os.path.join(assetOutPath, atlasName + '.png')])
+
 			# make json
 			for image_rect in atlas.image_rect_list:
 				width, height = (image_rect.width, image_rect.height) if not image_rect.rotated \
