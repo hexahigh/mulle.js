734ed6df956e071f46d92bd60ac2ed61e264b172
datagutten <datagutten@datagutten.net>
2020-07-21 16:05:38 +0200

    Change package root

--- deployment/package.sh
+++ deployment/package.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-tar -czf ~/Mulle_${BUILD_LANG}.tgz ${TRAVIS_BUILD_DIR}/dist
-tar -czf ~/Mulle_cst_${BUILD_LANG}.tgz ${TRAVIS_BUILD_DIR}/cst_out_new
+tar -czf ~/Mulle_${BUILD_LANG}.tgz -C ${TRAVIS_BUILD_DIR}/dist .
+tar -czf ~/Mulle_cst_${BUILD_LANG}.tgz -C ${TRAVIS_BUILD_DIR}/cst_out_new .
