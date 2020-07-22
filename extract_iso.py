103b9241503e2c06c3bdd8e0de2bba738acc680d
datagutten <datagutten@datagutten.net>
2020-07-22 17:52:41 +0200

    Correct path after move

--- extract_iso.py
+++ extract_iso.py
@@ -5,7 +5,7 @@ from pycdlib.dr import DirectoryRecord
 import pycdlib
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-movies_path = os.path.realpath(os.path.join(dir_path, '..', 'Movies'))
+movies_path = os.path.realpath(os.path.join(dir_path, 'Movies'))
 if not os.path.exists(movies_path):
     os.mkdir(movies_path)
 
@@ -26,5 +26,4 @@ for child in iso.list_children(iso_path='/Movies'):
     file = iso.full_path_from_dirrecord(child)
     extracted_file = os.path.join(movies_path, os.path.basename(file).upper())
     iso.get_file_from_iso(extracted_file, iso_path=file)
-    os.chdir(os.path.join(dir_path, '..'))
     main(['-e', '-i', extracted_file])
