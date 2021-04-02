7c507790c89b74824151ff772d0495b9481dedca
datagutten <datagutten@datagutten.net>
2021-04-02 12:23:56 +0200

    Use packages.config for Windows dependencies

--- deployment/windows_dependencies.cmd
+++ deployment/windows_dependencies.cmd
@@ -1,6 +1,1 @@
-choco install nodejs --version 10.21.0
-choco install imagemagick --version 7.0.10.19
-choco install python --version 3.8
-choco install optipng
-choco install ffmpeg
 copy deployment\ffmpeg_dll\*.dll %WINDIR%\System32
