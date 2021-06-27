0c337407e0447dc4e303028a4353497024dc2d50
datagutten <datagutten@datagutten.net>
2021-06-27 17:20:16 +0200

    Quiet download and cache imagemagick

--- deployment/imagemagick.sh
+++ deployment/imagemagick.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 mkdir bin
-wget -nc --no-check-certificate -O bin/magick https://imagemagick.org/download/binaries/magick
+wget -nc --no-check-certificate -Q -O bin/magick https://imagemagick.org/download/binaries/magick
 chmod +x bin/magick
 export PATH="$PWD/bin:$PATH"
 magick -version
