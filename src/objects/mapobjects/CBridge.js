77c7c5c74508b9371e003e5c76334bbf6b6b3cd0
datagutten <datagutten@datagutten.net>
2021-06-28 09:27:26 +0200

    Implement automatic bridge

--- src/objects/mapobjects/CBridge.js
+++ src/objects/mapobjects/CBridge.js
@@ -1,13 +1,24 @@
 'use strict'
 
+// noinspection JSValidateTypes
+/**
+ * Automatic bridge (map 27)
+ * @type {MulleMapObject}
+ */
 var MapObject = {}
 
 MapObject.onCreate = function () {
-  console.error('unfinished object', this.id, this)
+  this.animationHelper.add('normal', 'normal', this.opt.Direction, 3, true, true)
+  this.animations.play('normal')
+  this.game.mulle.playAudio(this.def.Sounds[0])
 }
 
-MapObject.onEnterInner = function () {
-
+MapObject.onEnterOuter = function (car) {
+  console.log(this.animations.frame)
+  if (this.animations.frame > 37) {
+    car.speed = 0
+    car.stepback(2)
+  }
 }
 
 export default MapObject
