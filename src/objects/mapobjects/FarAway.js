3c2f8db6a61866476c2ecaf6e9fc033aa3f87e21
datagutten <datagutten@datagutten.net>
2021-06-28 09:43:03 +0200

    Award medal when driving far away

--- src/objects/mapobjects/FarAway.js
+++ src/objects/mapobjects/FarAway.js
@@ -1,13 +1,18 @@
 'use strict'
 
+// noinspection JSValidateTypes
+/**
+ * Far away (Doris Digital, map 17)
+ * @type {MulleMapObject}
+ */
 var MapObject = {}
 
-MapObject.onCreate = function () {
-  console.error('unfinished object', this.id, this)
-}
-
 MapObject.onEnterInner = function () {
-
+  // TODO: Far away sound
+  const hasMedal = this.game.mulle.user.Car.hasMedal(2)
+  if (!hasMedal) {
+    this.game.mulle.user.Car.addMedal(2)
+  }
 }
 
 export default MapObject
